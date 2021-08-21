4bd17c8421d20b9c11d0d6fc5bd9fbea3ed7bc57
Юханссон М./Yukhansson M <61913044+MihahaUVP@users.noreply.github.com>
2021-08-21 12:48:40 +0300

    add project
    project show simple semaphores

---
+++ pp_lab2/pp_lab2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+namespace pp_lab2
+{
+    class Program
+    {
+        
+        private string[] array;
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 6; i++)
+            {
+                Reader reader = new Reader(i);
+            }
+            Console.ReadLine();
+        }
+    }
+    class Reader
+    {
+        static Semaphore sem = new Semaphore(1,1);
+        Thread myThread;
+        int count = 3;
+        public Reader(int i)
+        {
+            myThread = new Thread(Read);
+            myThread.Name = $"Читающий {i.ToString()}";
+            myThread.Start();
+        }
+        public void Read()
+        {
+            while (count>0)
+            {
+                sem.WaitOne();
+
+                Console.WriteLine($"{Thread.CurrentThread.Name} входит в  библиотеку");
+                Console.WriteLine($"{Thread.CurrentThread.Name} совершает акт чтения");
+                Thread.Sleep(1000);
+
+                Console.WriteLine($"{Thread.CurrentThread.Name} заканчивает своё пребывание в доме мудрости");
+                sem.Release();
+                count--;
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
